b9866c06c73f2f77cfa29490f94100d2bb411716
ABDUL MANAP <11210195@bsi.ac.id>
2025-06-23 07:35:14 +0700

    update halaman depan

--- src/consts.ts
+++ src/consts.ts
@@ -1,10 +1,10 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'ProtectQube - IoT Based Security Solution';
+export const SITE_TITLE = 'protectQube - IoT Based Security Solution';
 export const SITE_DESCRIPTION = 'Welcome to protectQube, Your Reliable IoT-Based Security Solution Ensure the safety of your devices with real-time monitoring and customizable security features. Protect hard-to-reach and widely dispersed equipment with ease. ';
 export const SITE_TITLE_CONTACT = 'Contact Us';
 export const SITE_DESCRIPTION_CONTACT = 'Contact Us for technical management services.';
 
-export const SITE_TITLE_BLOG = 'ProtectQube Blog';
-export const SITE_DESCRIPTION_BLOG = 'Stay updated with the latest news and insights from ProtectQube. Explore our blog for articles on IoT security, technology trends, and more.';
+export const SITE_TITLE_BLOG = 'protectQube Blog';
+export const SITE_DESCRIPTION_BLOG = 'Stay updated with the latest news and insights from protectQube. Explore our blog for articles on IoT security, technology trends, and more.';
